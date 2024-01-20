cb8a26e1d3e98fe9580905fa946c1ee0e036ffe6
alexkarmhon <89stigmat89@gmail.com>
2024-01-20 15:24:57 +0200

    .++

--- src/js/weather.js
+++ src/js/weather.js
@@ -1,10 +1,42 @@
 const API_KEY = '531234909003fb2cf97fea2704db1ea5';
-const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?q=berlin&appid=531234909003fb2cf97fea2704db1ea5';
+const BASE_URL = 'https://api.openweathermap.org/data/2.5';
 
-async function getWeather() {
-  const resp = await fetch(BASE_URL);
+const searchForm = document.querySelector('#search-form');
+const weatherCard = document.querySelector('#weather');
+const errorMsg = document.querySelector('#error');
+
+errorMsg.classList.add('is-hidden');
+
+
+
+async function getWeather(e) {
+  e.preventDefault();
+
+  const city = e.target.elements.cityname.value;
+  const resp = await fetch(`${BASE_URL}/weather?q=${city}&units=metric&appid=${API_KEY}`);
+
+  if (!resp.status.ok || !city) {
+    errorMsg.classList.remove('is-hidden');
+    setTimeout(() => {
+      errorMsg.classList.add('is-hidden');  
+    }, 3000);
+    return;
+  };
+
   const data = await resp.json()
   console.log(data);
+
+  document.querySelector('#weather__temp').innerHTML = Math.round(data.main.temp) + '&#8451';
+  document.querySelector('#weather__city').innerHTML = data.name;
+  document.querySelector('#humidity').innerHTML = data.main.humidity + '%';
+  document.querySelector('#wind-speed').innerHTML = data.wind.speed + 'km/h';
+  document.querySelector('#weather-icon-path').attributes.href.textContent = `/weather-icons.2c0c02e3.svg#${data.weather[0].main}`
+
+  weatherCard.classList.remove('is-hidden');
+  searchForm.reset();
 }
 
-getWeather()
+searchForm.addEventListener('submit', getWeather)
+
+// /weather-icons.2c0c02e3.svg#cloud-snowflakes
+// /weather-icons.2c0c02e3.svg#Clouds
